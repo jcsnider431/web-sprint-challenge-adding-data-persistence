a010a292a78e7f7851fd2adda6ec92f209fbc9f5
Chase Snider <jcsnider@outlook.com>
2021-03-19 13:40:22 -0400

    Slowly but surely

--- data/migrations/20210319160227_project_table.js
+++ data/migrations/20210319160227_project_table.js
@@ -2,19 +2,19 @@ exports.up = function(knex) {
     return knex.schema
     .createTable("projects", (table) => {
       table.increments("project_id");
-      table.string("project_name", 128).unique().notNullable();
+      table.string("project_name").notNullable();
       table.string("project_description");
       table.boolean("project_completed").defaultTo(false);
     })
     .createTable("resources", (table) => {
       table.increments("resource_id");
-      table.string("resource_name", 128).unique().notNullable();
+      table.string("resource_name").unique().notNullable();
       table.string("resource_description");
     })
     .createTable("tasks", (table) => {
       table.increments("task_id");
       table.string("task_description").notNullable();
-      table.string("task_notes", 128);
+      table.string("task_notes");
       table.boolean("task_completed").defaultTo(false);
       table
         .integer("project_id")
